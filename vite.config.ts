9df2aa80932303899650c4c987b42beb4aafff0c
abdesettar senouci <118857554+abdesettar-senouci@users.noreply.github.com>
2025-01-25 20:27:56 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: [
+      'frontend-rexo.onrender.com'
+    ]
     port: 5173,
     host: true,
     proxy: {
